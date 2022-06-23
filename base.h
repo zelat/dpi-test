90a043d23cf1a058876dd6c0cb9dfe25ba30c642
Chao Tan <tanchao@huayun.com>
2022-06-23 10:00:25 +0800

    add urcu  function

---
+++ base.h
@@ -0,0 +1,17 @@
+//
+// Created by Administrator on 2022/6/23.
+//
+
+#ifndef DPI_TEST_BASE_H
+#define DPI_TEST_BASE_H
+
+#include <inttypes.h>
+
+#ifndef likely
+# define likely(x)        __builtin_expect(!!(x), 1)
+#endif
+#ifndef unlikely
+# define unlikely(x)        __builtin_expect(!!(x), 0)
+#endif
+
+#endif //DPI_TEST_BASE_H
